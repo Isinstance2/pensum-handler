3ea28c7080efd9cdd50029723b1521537af7ed31
Isinstance2 <joanjulian2@gmail.com>
2025-07-19 01:06:30 -0400

    restructured data ingestion option

--- data/init_db.py
+++ data/init_db.py
@@ -8,6 +8,8 @@ import unicodedata
 
 
 
+
+
 # Define el formato con timestamp
 log_format = "%(asctime)s - %(levelname)s - %(message)s"
 
@@ -34,29 +36,30 @@ logging.getLogger().addHandler(console_handler)
 
 class PensumLoaderUnicaribe():
     def __init__(self, file_name):
-        self.data_path = os.path.join(os.getcwd(), "data", f"{file_name}.pdf")
+        self.data_path = os.path.join(r'/home/oa/projects/uni/data', f"{file_name}")
+        self.data_folder = r'/home/oa/projects/uni/data'
+        self.original_name = file_name
+        self.updated_path = os.path.join(self.data_folder, f"{file_name}_updated.csv")
 
         if not os.path.exists(self.data_path):
             raise FileNotFoundError(f"No se encontró el archivo: {self.data_path}")
+
         
-        self.pattern = r"([A-Z]{3,}-\d{3})\s+(.+?)\s+(\d{1,2})\s+([A-Z\-0-9,]*)"
-        self.data = []
-        with pdfplumber.open(self.data_path) as pdf:
-            full_text = ""
-            for page in pdf.pages:
-                full_text += page.extract_text() + "\n"
-        matches = re.findall(self.pattern, full_text)
-        for match in matches:
-            clave, nombre, credito,pre_req = match
-            self.data.append({
-            "clave" : clave.strip(),
-            "asignatura": nombre.strip(),
-            "credito" : credito.strip(),
-            "pre_req" : pre_req.strip()
-            })
-        self.df = pd.DataFrame(self.data)
-        self.df = self.format_data(self.df)
+        try:
+            self.df = pd.read_csv(self.updated_path)
+            self.df = self.format_data(self.df)
+            logging.info(f"Loaded updated data from: {self.updated_path}")
         
+        except Exception as e:
+            logging.error(f"Failed to load updated data, falling back to PDF: {e}")
+            self.df = self.load_from_pdf_and_format()
+    
+            
+            
+        
+
+    def save(self):
+        self.df.to_csv(self.updated_path, index=False)
 
     def remove_accents(self,input_str):
         # Normaliza a forma decomposed (NFD), separa letras de acentos y elimina los acentos
@@ -64,20 +67,55 @@ class PensumLoaderUnicaribe():
         only_ascii = "".join([c for c in nfkd_form if not unicodedata.combining(c)])
         return only_ascii
 
+    def load_from_pdf_and_format(self):
+        self.pattern = r"([A-Z]{3,}-\d{3})\s+(.+?)\s+(\d{1,2})\s+([A-Z\-0-9,]*)"
+        self.data = []
+        with pdfplumber.open(self.data_path) as pdf:
+            full_text = "\n".join(page.extract_text() for page in pdf.pages)
+        matches = re.findall(self.pattern, full_text)
+        for match in matches:
+            clave, nombre, credito, pre_req = match
+            self.data.append({
+                "clave": clave.strip(),
+                "asignatura": nombre.strip(),
+                "credito": credito.strip(),
+                "pre_req": pre_req.strip()
+            })
+        df = pd.DataFrame(self.data)
+        return self.format_data(df)
+
 
     def format_data(self, df):
-        self.df['completo'] = pd.Series([False]*len(self.df), dtype='bool')  # default False
-        self.df['mes_cursado'] = pd.Series([pd.NaT]*len(self.df), dtype='datetime64[ns]')
-        self.df['nota'] = pd.Series([np.nan]*len(self.df), dtype='float')
-        self.df['clave'] = self.df['clave'].astype(str)
-        self.df['asignatura'] = self.df['asignatura'].astype(str)
-        self.df['credito'] = self.df['credito'].astype(int)
-        self.df['pre_req'] = self.df['pre_req'].astype(str)
-        empty_req = self.df['pre_req'] == '-'
-        self.df.loc[empty_req, 'pre_req'] = "" 
+        df['completo'] = pd.Series([False]*len(df), dtype='bool')  # default False
+        df['mes_cursado'] = pd.Series([pd.NaT]*len(df), dtype='datetime64[ns]')
+        df['nota'] = pd.Series([np.nan]*len(df), dtype='float')
+        df['clave'] = df['clave'].astype(str)
+        df['asignatura'] = df['asignatura'].astype(str)
+        df['credito'] = df['credito'].astype(int)
+        df['pre_req'] = df['pre_req'].astype(str)
+        empty_req = df['pre_req'] == '-'
+        df.loc[empty_req, 'pre_req'] = "" 
+        return df
+    
+    def __str__(self):
         return self.df
     
-    def add_record(self, subject, month, grade: float):
+    def completed_summary(self):
+        avg = self.df['nota'].mean()
+        total = len(self.df)
+        completed_x = len(self.df[self.df['completo'] == True])
+        missing = len(self.df[self.df['completo'] != True])
+
+        mapping = {
+            'avg' : avg,
+            'total' : total,
+            'completed' : completed_x,
+            'missing' : missing
+        }
+        return mapping
+
+    
+    def edit_record(self, subject, month, grade: float):
         try:
             # Aplica remove_accents a toda la columna solo una vez y guarda en columna auxiliar
             if 'asignatura_limpia' not in self.df.columns:
@@ -100,6 +138,7 @@ class PensumLoaderUnicaribe():
             idx = matches.index[0]
             self.df.at[idx, 'nota'] = grade
             self.df.at[idx, 'mes_cursado'] = month
+            self.df.at[idx, 'completo'] = True
             logging.info(f"Assigned grade {grade} to subject: {self.df.at[idx, 'asignatura']}")
     
         except Exception as e:
@@ -119,6 +158,15 @@ class PensumLoaderFactory:
         else:
             raise ValueError("Universidad no soportada.")
 
-if __name__ == '__main__':
+
+if __name__ == '__main__':   
     loader = PensumLoaderFactory.get_loader('unicaribe', 'PENSUM-Ingenieria-de-Datos-e-Inteligencia-Organizacional')
-    print(loader.df.head())  # Para chequear que cargó bien
+    print(loader.df) 
+
+
+
+
+
+
+
+
--- scripts/pensum_tui.py
+++ scripts/pensum_tui.py
@@ -1,11 +1,18 @@
 # pensum_tui.py
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Button, Static
+from textual.widgets import Input, Label
 from textual.containers import Vertical
-from pensum_handler import PensumHandler
 from textual.screen import Screen
 from textual.containers import Horizontal
-uni = PensumHandler()
+from data.init_db import PensumLoaderFactory
+from datetime import datetime
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+data_folder = r'/home/oa/projects/uni/data'
 
 
 class PensumApp(App):
@@ -32,55 +39,91 @@ class PensumApp(App):
     padding: 1 2;
     border: black;
     color: yellow;
-    }             
+    }
+    
+    .edit-btn {
+    background: $boost;
+    color: $text;
+    padding: 1 2;
+    border: round yellow;
+    text-style: bold;
+    }
+
+    .edit-btn:hover {
+    background: $primary;
+    color: $text;
+    }
     """
 
-
-
+    def __init__(self):
+        super().__init__()
+        self.files = []
+    
     def compose(self) -> ComposeResult:
-        yield Header(show_clock=True)
-        summary = uni.completed_summary()
-        text = (
-        f"📘 PENSUM SUMMARY\n"
-        f"─────────────────────\n"
-        f"🟢 Total Subjects: {summary['total']}\n"
-        f"✅ Completed: {summary['completed']}\n"
-        f"❌ Missing: {summary['missing']}\n"
-    )
-        yield Static(text, id="summary_box")
-        
+        yield Header(show_clock=True)  
         with Vertical():
-            yield Button("📋 View Courses", id="view")
-            yield Button("✅ Mark Completed", id="complete")
+            yield Static("Selecciona universidad:")
+            yield Button("Unicaribe", id="unicaribe")
             yield Button("🚪 Exit", id="exit")
         yield Footer()
 
     def on_button_pressed(self, event : Button.Pressed):
-        match event.button.id:
-            case "view":
-                self.view_courses_screen()
+        if event.button.id == "unicaribe":
+            self.push_screen(FileSelectionScreen())
 
-    def view_courses_screen(self):
+
+class FileSelectionScreen(Screen):
+    BINDINGS = [("escape", "app.pop_screen", "Back")]
+
+    def compose(self) -> ComposeResult:
+        yield  Header()
+        yield Static("Selecciona el archivo pdf del pensum (escribe el numero):")
+        self.files = [f for f in os.listdir(data_folder) if f.endswith(".pdf")]
+
+        for idx, fname in enumerate(self.files):
+            yield Static(f"[{idx}] {fname}")
+        
+        yield Input(placeholder="numero del archivo", id="file_number")
+        yield Footer()
     
-        uni = PensumHandler()
+    
 
-        data = uni.display_table("courses")  # This is your DB call
-        output = "\n".join(str(row) for row in data)
-        summary = uni.completed_summary()
-        summary_text = (
-        f"📘 PENSUM SUMMARY\n"
-        f"─────────────────────\n"
-        f"🟢 Total Subjects: {summary['total']}\n"
-        f"✅ Completed: {summary['completed']}\n"
-        f"❌ Missing: {summary['missing']}\n"
-        f"📊 avg: {summary['avg']}\n"
-        f"{summary['remaining']}\n"
+    def on_input_submitted(self, event: Input.Submitted) -> None:
+        try:
+            index = int(event.value)
+            file_name = self.files[index]
+            
+            loader = PensumLoaderFactory.get_loader('unicaribe', os.path.join(data_folder, file_name))
+            df = loader.df
+            summary = loader.completed_summary()
 
-    )
+            today = datetime.today()
+            target_date = datetime.strptime("2028-03-04", "%Y-%m-%d")
+            delta = target_date - today
+            years = delta.days // 365
+            months = (delta.days % 365) // 30
+            days = (delta.days % 365) % 30
 
-        self.push_screen(CourseScreen(content=output, summary_text=summary_text))
+            summary_text = (
+                f"📘 PENSUM SUMMARY\n"
+                f"─────────────────────\n"
+                f"🟢 Total Subjects: {summary['total']}\n"
+                f"✅ Completed: {summary['completed']}\n"
+                f"❌ Missing: {summary['missing']}\n"
+                f"📊 Avg Grade: {summary['avg']:.2f}\n"
+                f"📅 Time Left: {years} yrs, {months} mo, {days} days"
+            )
 
-class CourseScreen(Screen):
+            file_output = str(df)
+
+            self.app.push_screen(UnicaribeScreen(file_output, summary_text))
+
+        except (ValueError, IndexError) as e:
+            self.app.bell()
+            self.query_one(Input).placeholder = "❌ Entrada inválida. Intenta de nuevo."
+
+
+class UnicaribeScreen(Screen):
     BINDINGS = [("escape", "app.pop_screen", "Back")]
 
     def __init__(self, content: str, summary_text : str) -> None:
@@ -90,9 +133,14 @@ class CourseScreen(Screen):
 
     def compose(self) -> ComposeResult:
         yield Header()
+
         with Horizontal():
             yield Static(self.content, id="course_list")
-            yield Static(self.summary_text, id="summary_box")
+
+            with Vertical(id="summary_container"):
+                yield Static(self.summary_text, id="summary_box")
+                yield Button("✏️ Edit Record", id="edit_record", classes="edit-btn")
+
         yield Button("🔙 Back", id="back")
         yield Footer()
 
